15393bdbf6a8ecbd910596ba07e0d7f594cf2fbb
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2016-12-22 23:52:41 +0530

    wrapper: Reverting write call changes for N-MR1
    Removing write call changes in '__write_chk.cpp', as this file
    is deleted in N-MR1.
    
    Change-Id: Ibe1659641b53fb1d7e84ca237631128dc9620109
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- libc/Android.mk
+++ libc/Android.mk
@@ -1022,12 +1022,6 @@ LOCAL_CFLAGS := $(libc_common_cflags) \
 LOCAL_CONLYFLAGS := $(libc_common_conlyflags)
 LOCAL_CPPFLAGS := $(libc_common_cppflags) -Wold-style-cast
 
-ifeq ($(BOARD_USES_QCNE),true)
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CFLAGS += -DUSE_WRAPPER
-endif
-endif
-
 LOCAL_C_INCLUDES := $(libc_common_c_includes) bionic/libstdc++/include
 LOCAL_MODULE := libc_bionic
 LOCAL_CLANG := $(use_clang)
@@ -1055,20 +1049,8 @@ LOCAL_SRC_FILES := $(libc_bionic_ndk_src_files)
 LOCAL_CFLAGS := $(libc_common_cflags) \
     -Wframe-larger-than=2048
 
-ifeq ($(BOARD_USES_QCNE),true)
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CFLAGS += -DUSE_WRAPPER
-endif
-endif
-
 LOCAL_CONLYFLAGS := $(libc_common_conlyflags)
 LOCAL_CPPFLAGS := $(libc_common_cppflags) -Wold-style-cast \
-
-ifeq ($(BOARD_USES_QCNE),true)
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CPPFLAGS += -DUSE_WRAPPER
-endif
-endif
 
 LOCAL_C_INCLUDES := $(libc_common_c_includes) bionic/libstdc++/include
 LOCAL_MODULE := libc_bionic_ndk
@@ -1224,12 +1206,6 @@ LOCAL_SRC_FILES_arm += \
 LOCAL_CFLAGS := $(libc_common_cflags) \
     -DLIBC_STATIC \
 
-ifeq ($(BOARD_USES_QCNE),true)
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CFLAGS += -DUSE_WRAPPER
-endif
-endif
-
 LOCAL_WHOLE_STATIC_LIBRARIES := \
     libc_bionic_ndk \
     libc_cxa \
@@ -1263,12 +1239,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(libc_common_src_files)
 LOCAL_CFLAGS := $(libc_common_cflags)
-
-ifeq ($(BOARD_USES_QCNE),true)
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CFLAGS += -DUSE_WRAPPER
-endif
-endif
 
 LOCAL_CONLYFLAGS := $(libc_common_conlyflags)
 LOCAL_CPPFLAGS := $(libc_common_cppflags)
@@ -1414,13 +1384,6 @@ LOCAL_SRC_FILES := \
     bionic/libc_init_dynamic.cpp \
     bionic/NetdClient.cpp \
     arch-common/bionic/crtend_so.S \
-
-ifeq ($(BOARD_USES_QCNE),true)
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-    LOCAL_SRC_FILES += codeaurora/PropClient.cpp
-    LOCAL_CFLAGS += -DUSE_WRAPPER
-endif
-endif
 
 LOCAL_MODULE := libc
 LOCAL_CLANG := $(use_clang)
